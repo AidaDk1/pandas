db9f882b11251ee39810143a0f8683c667030709
AidaDk1 <65682383+AidaDk1@users.noreply.github.com>
2022-05-12 18:37:39 +0600

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -86,7 +86,10 @@ df5=pd.DataFrame(data5,index=['first','second'])
 print(df5['name'])
 
 
-
+data = {'Item1' : pd.DataFrame(np.random.randn(4, 3)), 
+   'Item2' : pd.DataFrame(np.random.randn(4, 2))}
+p = pd.Panel(data)
+print p.major_xs(1)
 
 
 
